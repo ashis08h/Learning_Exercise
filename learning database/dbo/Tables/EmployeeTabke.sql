59be5647218e1e93d6dc15949e487b273955201c
ashis08h <ashis08h@gmail.com>
2022-07-28 17:01:28 +0530

    dgd

--- learning database/dbo/Tables/EmployeeTabke.sql
+++ learning database/dbo/Tables/EmployeeTabke.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[EmployeeTabke] (
     [Id]     INT           NOT NULL,
     [Name]   VARCHAR (100) NOT NULL,
-    [Dob]    VARCHAR (100) NULL,
+    [Dob]    VARCHAR (100) NOT NULL,
     [Salary] VARCHAR (100) NULL
 );
 
