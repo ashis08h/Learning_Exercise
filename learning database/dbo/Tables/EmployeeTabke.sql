66e74d736da5cf8a28fc4cbe2614cbb426153dec
ashis08h <ashis08h@gmail.com>
2022-07-28 16:59:05 +0530

    minor changed in bn

--- learning database/dbo/Tables/EmployeeTabke.sql
+++ learning database/dbo/Tables/EmployeeTabke.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[EmployeeTabke] (
     [Id]     INT           NOT NULL,
-    [Name]   VARCHAR (100) NULL,
+    [Name]   VARCHAR (100) NOT NULL,
     [Dob]    VARCHAR (100) NULL,
     [Salary] VARCHAR (100) NULL
 );
